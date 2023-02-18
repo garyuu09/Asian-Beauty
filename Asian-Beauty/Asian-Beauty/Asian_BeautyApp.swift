95888a0669083b033f831091fca4d5f7887d17ef
iru <jinsei100nen.douikiruka@gmail.com>
2023-02-18 22:26:38 +0900

    no message

---
+++ Asian-Beauty/Asian-Beauty/Asian_BeautyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asian_BeautyApp.swift
+//  Asian-Beauty
+//
+//  Created by 松尾健司 on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct Asian_BeautyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
