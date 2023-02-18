bd2a34660dcb3cbed65d3b15e7aa015bb9076c3c
iru <jinsei100nen.douikiruka@gmail.com>
2023-02-18 23:03:31 +0900

    secondView

---
+++ Asian-Beauty/Asian-Beauty/SecondView.swift
@@ -0,0 +1,27 @@
+//
+//  SecondView.swift
+//  Asian-Beauty
+//
+//  Created by 松尾健司 on 2023/02/18.
+//
+
+import Foundation
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
