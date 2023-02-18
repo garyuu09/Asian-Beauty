2fa9e3555e32d624b6734b51597010a2f656b7da
iru <jinsei100nen.douikiruka@gmail.com>
2023-02-19 08:29:46 +0900

    #4 レイアウト

--- Asian-Beauty/Asian-Beauty/SecondView.swift
+++ Asian-Beauty/Asian-Beauty/SecondView.swift
@@ -11,16 +11,42 @@ import SwiftUI
 struct SecondView: View {
     var body: some View {
         VStack {
-            HStack{
-                Text("カンボジア")
-                    .multilineTextAlignment(.leading)
-                Spacer()
+//                現在Viewには、最大10個しかView(Textなど）を追加することが出来ない。
+//                その為、10個以上追加したい場合は、Groupで囲うことで、
+//                Groupを1個のViewとして認識してくれ、見た目上10個以上のViewを置くことができる。
+            //国
+            Group{
+                HStack{
+                    Text("カンボジア")
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                }
+                Image("アンコールワット")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
             }
-            Image("アンコールワット")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Text("Hello, world!")
-            
+            //見出し１
+            Group{
+                HStack{
+                    Text("見出し１")
+                    Spacer()
+                }
+                Divider()
+                Text("情報1")
+                Text("情報2")
+                Text("情報3")
+                Divider()
+            }
+            //見出し２
+            Group{
+                HStack{
+                    Text("見出し2")
+                    Spacer()
+                }
+                Text("情報4")
+                Text("情報5")
+                Text("情報6")
+            }
         }
         .padding()
     }
