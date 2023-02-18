73ba6145776986aec04c88b585d083f3bc625fec
iru <jinsei100nen.douikiruka@gmail.com>
2023-02-18 23:05:02 +0900

    gggg

--- Asian-Beauty/Asian-Beauty/SecondView.swift
+++ Asian-Beauty/Asian-Beauty/SecondView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SecondView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "ggggg")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
